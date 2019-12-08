b96ec1e05ed81847df6bfe54f2698058d366e15e
platon517 <platon.517@gmail.com>
2019-12-08 12:46:31 +0300

    express

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ server });
 
 const users = new Set();
 
